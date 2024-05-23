8a44bd6402a377323cff5fcff8e131f7c584349f
Mohd Yaseen Khan <167443536+MyNameisMohdYaseenKhan@users.noreply.github.com>
2024-05-23 21:16:11 +0530

    first commit

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main{
 		public static void main(String args[]){
 			System.out.println("My name  is mohd yaseen khan");	
+			System.out.println("oh wow This is running on cloud");	
+			
 		}
 }
